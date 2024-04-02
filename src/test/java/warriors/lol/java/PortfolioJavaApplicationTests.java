20a2a2f9afdf58fc9158cc41c657ab682262ccac
Alberto Souza <albertosouza16.contato@gmail.com>
2024-04-01 21:28:21 -0300

    first commit

---
+++ src/test/java/warriors/lol/java/PortfolioJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package warriors.lol.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
